a5ed6853491db9fbadc3955c712fe5442ffbc469
SukaziN <nkosiyazisukazie@gmail.com>
2026-06-12 11:50:49 +0200

    selected page elements as objects

---
+++ script.js
@@ -0,0 +1,5 @@
+let email = document.getElementById("email").value;
+let password = document.getElementById("password").value;
+let username = document.getElementById("username").value;
+let button = document.getElementById("submit");
+let errorAlert = document.getElementsByClassName("error");
