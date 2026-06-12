6f0f955ced40f4f4448449130f4bd73e2cc8f83e
SukaziN <nkosiyazisukazie@gmail.com>
2026-06-12 14:21:50 +0200

    converted if else statements to ternary operators for more concise code

--- script.js
+++ script.js
@@ -9,26 +9,22 @@ function validateInput() {
   let password = document.getElementById("password").value;
   let username = document.getElementById("username").value;
 
-  if (!email.match(validEmail)) {
-    console.log(email + " please enter a valid email address!");
-  } else {
-    console.log("We will send a weekly newsletter to " + email);
-  }
-  if (password.match(validPassword) == null || password.length < 8) {
-    console.log(
-      password +
-        " please enter a strong password! Pro-Tip: a mixture of lowercase and uppercase letters, numbers, and special characters.",
-    );
-  } else {
-    console.log("Please confirm your password below.");
-  }
-  if (username.match(specialCharacters) == null) {
-    console.log("Welcome aboard " + username);
-  } else {
-    console.log(
-      username +
-        " username cannot contain spaces, numbers or special characters.",
-    );
-  }
+  !email.match(validEmail)
+    ? console.log(email + " please enter a valid email address!")
+    : console.log("We will send a weekly newsletter to " + email);
+
+  password.match(validPassword) == null || password.length < 8
+    ? console.log(
+        password +
+          " please enter a strong password! Pro-Tip: a mixture of lowercase and uppercase letters, numbers, and special characters.",
+      )
+    : console.log("Please confirm your password below.");
+
+  username.match(specialCharacters) == null
+    ? console.log("Welcome aboard " + username)
+    : console.log(
+        username +
+          " username cannot contain spaces, numbers or special characters.",
+      );
 }
 button.addEventListener("click", validateInput);
