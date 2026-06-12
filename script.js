355660ee8771b5fc79cf81fcfe2abcfc04c9fc4f
SukaziN <nkosiyazisukazie@gmail.com>
2026-06-12 12:07:24 +0200

    created a function and event handler

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-let email = document.getElementById("email").value;
-let password = document.getElementById("password").value;
-let username = document.getElementById("username").value;
 let button = document.getElementById("submit");
 let errorAlert = document.getElementsByClassName("error");
+/*for (let i = 0; i < errorAlert.length; i++) {
+  errorAlert.classList.add("hide");
+}*/
+function validateInput() {
+  let email = document.getElementById("email").value;
+  console.log(email);
+  let password = document.getElementById("password").value;
+  console.log(password);
+  let username = document.getElementById("username").value;
+  console.log(username);
+}
+button.addEventListener("click", validateInput);
