8a7b0ecf8f640723989838cc040df27762595141
SukaziN <nkosiyazisukazie@gmail.com>
2026-06-12 12:40:01 +0200

    stored user input values in an array and looped through the array to check if defined conditions are met

--- script.js
+++ script.js
@@ -8,16 +8,29 @@ let validUserName = /([a-zA-Z]+)/g;
 let validPassword = /([a-zA-Z0-9.-_@\/])/g;
 function validateInput() {
   let email = document.getElementById("email").value;
-  console.log(email);
   let password = document.getElementById("password").value;
-  console.log(password);
   let username = document.getElementById("username").value;
-  console.log(username);
 
-  if (!email.match(validEmail)) {
-    console.log(email + " please enter a valid email address!");
-  } else {
-    console.log("We will send a weekly newsletter to " + email);
+  let userInfo = [];
+  userInfo.push(email);
+  userInfo.push(password);
+  userInfo.push(username);
+  console.log(userInfo);
+
+  for (let i = 0; i < userInfo.length; i++) {
+    if (!email.match(validEmail)) {
+      console.log(email + " please enter a valid email address!");
+    } else {
+      console.log("We will send a weekly newsletter to " + email);
+    }
+    if (!password.match(validPassword)) {
+      console.log(
+        password +
+          " please enter a strong password! Pro-Tip: a mixture of lowercase and uppercase letters, numbers, and special characters.",
+      );
+    } else {
+      console.log("Please confirm your password below.");
+    }
   }
 }
 button.addEventListener("click", validateInput);
