4359ab257355c7224ef031f9d28127624265c982
SukaziN <nkosiyazisukazie@gmail.com>
2026-06-14 18:48:52 +0200

    removed hide class to display error message to user and removed error class to replace error message  in html with user interactive feedback. Also added some notes from observations during debugging

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let button = document.getElementById("submit");
-let errorAlert = document.getElementsByClassName("error");
-
+let errorAlert = document.querySelector(".error");
 let validEmail = /([a-zA-Z0-9.-_]+@[a-zA-Z0-9.-_]+\.[a-zA-Z0-9.-_]+)/g;
 let specialCharacters = /[\d\s-_&%#@!|]+/g;
 let validPassword = /([\d.-_]+)/g;
@@ -9,22 +8,40 @@ function validateInput() {
   let password = document.getElementById("password").value;
   let username = document.getElementById("username").value;
 
-  !email.match(validEmail)
-    ? console.log(email + " please enter a valid email address!")
-    : console.log("We will send a weekly newsletter to " + email);
+  if (!email.match(validEmail)) {
+    console.log(email + " please enter a valid email address!");
+    errorAlert.classList.remove("hide"); //if the className was used to select the element, classList does not work
+  } else {
+    console.log("We will send a weekly newsletter to " + email);
+    errorAlert.classList.remove("hide"); //when adding or removing classLists, you cannot pair them as you do in html. They must be coded individually
+    errorAlert.classList.remove("error");
+    errorAlert.textContent = "We will send a weekly newsletter to " + email;
+  } //you must prevent the default behavior of the form elements to clear fields to retain user interaction thereby improving responsiveness and reassuring the user
 
-  password.match(validPassword) == null || password.length < 8
-    ? console.log(
-        password +
-          " please enter a strong password! Pro-Tip: a mixture of lowercase and uppercase letters, numbers, and special characters.",
-      )
-    : console.log("Please confirm your password below.");
+  if (password.match(validPassword) == null || password.length < 8) {
+    console.log(
+      password +
+        " please enter a strong password! Pro-Tip: a mixture of lowercase and uppercase letters, numbers, and special characters.",
+    );
+    errorAlert.classList.remove("hide");
+  } else {
+    console.log("Please confirm your password below.");
+    errorAlert.classList.remove("error");
+    errorAlert.classList.remove("hide");
+    alert("Would you like to save your password?");
+  }
 
-  username.match(specialCharacters) == null
-    ? console.log("Welcome aboard " + username)
-    : console.log(
-        username +
-          " username cannot contain spaces, numbers or special characters.",
-      ); //username logic worked at first but now exclusively returns else statement (code is airtight, could be a device issue, run debug session after a while and maybe reboot session or device)
+  if (username.match(specialCharacters) == null) {
+    console.log("Welcome aboard " + username);
+    errorAlert.classList.remove("error");
+    errorAlert.classList.remove("hide");
+    errorAlert.textContent = "Welcome aboard " + username;
+  } else {
+    console.log(
+      username +
+        " username cannot contain spaces, numbers or special characters.",
+    );
+    errorAlert.classList.remove("hide");
+  }
 }
 button.addEventListener("click", validateInput);
