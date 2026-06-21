b63e4bebffee712793ed476ef66535c1672e4c69
SukaziN <nkosiyazisukazie@gmail.com>
2026-06-21 10:09:42 +0200

    removed redundant lines of code

--- script.js
+++ script.js
@@ -11,10 +11,8 @@ function validateEmailInput() {
   event.preventDefault();
   let email = document.getElementById("email").value;
   if (!email.match(validEmail)) {
-    console.log(email + " please enter a valid email address!");
     emailFeedback.classList.remove("hide"); //if the className was used to select the element, classList does not work
   } else {
-    console.log("We will send a weekly newsletter to " + email);
     emailFeedback.classList.remove("hide"); //when adding or removing classLists, you cannot pair them as you do in html. They must be coded individually
     emailFeedback.classList.remove("error");
     emailFeedback.textContent = "We will send a weekly newsletter to " + email;
@@ -26,16 +24,7 @@ function validatePasswordInput() {
   event.preventDefault();
   let password = document.getElementById("password").value;
   if (password.match(validPassword) == null || password.length < 8) {
-    console.log(
-      password +
-        " please enter a strong password! Pro-Tip: a mixture of lowercase and uppercase letters, numbers, and special characters.",
-    );
     passwordFeedback.classList.remove("hide");
-  } else {
-    console.log("Please confirm your password below.");
-    passwordFeedback.classList.remove("error");
-    passwordFeedback.classList.remove("hide");
-    alert("Would you like to save your password?");
   }
 }
 button.addEventListener("click", validatePasswordInput);
@@ -44,15 +33,8 @@ function validateUsername() {
   event.preventDefault();
   let username = document.getElementById("username").value;
   if (username.length >= 2 && username.match(specialCharacters) == null) {
-    console.log("Welcome aboard " + username);
-    usernameFeedback.classList.remove("error");
-    usernameFeedback.classList.remove("hide");
-    usernameFeedback.textContent = "Welcome aboard " + username;
+    alert("Welcome aboard " + username + "!");
   } else {
-    console.log(
-      username +
-        " username cannot be empty, contain spaces, numbers or special characters.",
-    );
     usernameFeedback.classList.remove("hide");
   }
 }
